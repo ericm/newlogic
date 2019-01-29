e75382e9860441097e6529659ebccb2f97351cc7
TooFiveFive <git@er1c.me>
2019-01-29 13:33:51 +0000

    small fixes

--- app/components/Workspace.tsx
+++ app/components/Workspace.tsx
@@ -1,22 +1,17 @@
 import * as React from 'react';
 import { WorkspaceProps, WorkspaceState } from '../interfaces/components';
 
-// import { Stage, Layer } from 'react-konva';
-
-// let styles = require('./styles/Workspace.scss');
+let styles = require('./styles/Workspace.scss');
 
 //import items (gates etc)
 // import AndGate from './items/AND';
 
 export default class Workspace extends React.Component<WorkspaceProps, WorkspaceState> {
 	public render() {
-		return (<div><p>i</p></div>)
-		// return (
-		// 	<Stage width={this.props.width} height={this.props.height} className={styles.main}>
-		// 		<Layer>
-		// 			<AndGate />
-		// 		</Layer>
-		// 	</Stage>
-		// )
+		return (
+			<div className={styles.main}>
+				{/* <canvas width="200" height="100"></canvas> */}
+			</div>
+		)
 	}
 }
