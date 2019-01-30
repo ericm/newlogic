784bcbe6731e9c275513908525b7f557b3a92680
TooFiveFive <git@er1c.me>
2019-01-30 21:35:36 +0000

    canvas function

--- app/components/Workspace.tsx
+++ app/components/Workspace.tsx
@@ -8,12 +8,30 @@ let styles = require('./styles/Workspace.scss');
 
 export default class Workspace extends React.Component<WorkspaceProps, WorkspaceState> {
 
+	canvas: HTMLCanvasElement
+
 	public constructor(props: WorkspaceProps) {
 		super(props);
 		this.state = {
 			width: (this.props.width * window.innerWidth / 100).toString(), 
 			height: (this.props.height * window.innerHeight / 100).toString()
 		}
+	}
+	
+	public componentDidMount() {
+		this.updateCanvas();
+	}
+	
+	public componentDidUpdate() {
+		this.updateCanvas();
+	}
+
+	private updateCanvas = () => {
+		const cont = this.canvas.getContext('2d');
+		if (cont !== null) {
+			const ctx: CanvasRenderingContext2D = cont;
+			ctx.fillText("Hello World", 10, 50);
+		}	
 	}
 
 	public resize = (n: Component): void => {
@@ -26,7 +44,8 @@ export default class Workspace extends React.Component<WorkspaceProps, Workspace
 	public render() {
 		return (
 			<div className={styles.main}>
-				<canvas className={styles.canvas} width={this.state.width} height={this.state.height}></canvas>
+				<canvas ref={(canvas) => {if (canvas !== null) this.canvas = canvas}} 
+					className={styles.canvas} width={this.state.width} height={this.state.height} />
 			</div>
 		)
 	}
