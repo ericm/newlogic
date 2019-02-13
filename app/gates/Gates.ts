9c42b6ff86896bbddab2a83ebd674d9a31aeaaee
TooFiveFive <git@er1c.me>
2019-02-13 12:53:41 +0000

    fixed click on gates

--- app/gates/Gates.ts
+++ app/gates/Gates.ts
@@ -11,8 +11,13 @@ export default class Gates<T> {
     }
 
     public click = (): void => {
-        this.ctx.rect(this.state.coords.x, this.state.coords.y, 
-            this.state.coords.x + this.state.size.width, this.state.coords.y + this.state.size.height);
-            this.ctx.stroke();
+        this.ctx.setLineDash([6]);
+        this.ctx.lineWidth = 1;
+        this.ctx.rect(this.state.coords.x-6, this.state.coords.y-6, 
+        this.state.size.width+12, this.state.size.height+12);
+        this.ctx.stroke();
+
+        this.ctx.lineWidth = 3;
+        this.ctx.setLineDash([0]);
     }
 }
