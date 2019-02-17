f2534f168d9beec9411d61ab1a52390b84487955
TooFiveFive <git@er1c.me>
2019-02-17 17:25:15 +0000

    unit tests for workspace

---
+++ test/components/Workspace.spec.tsx
@@ -0,0 +1,32 @@
+import '../utils/enzymeConfig';
+
+import { mount, ReactWrapper } from 'enzyme';
+import * as React from 'react';
+import Workspace from '../../app/components/Workspace';
+import * as sinon from 'sinon';
+import { WorkspaceState } from '../../app/interfaces/components';
+
+const el = (<Workspace width={100} height={100} />);
+
+describe("<Workspace />", () => {
+    it("calls componentDidMount", () => {
+        sinon.spy(Workspace.prototype, 'componentDidMount');
+        mount(el);
+        
+        expect(Workspace.prototype.componentDidMount).toHaveProperty('callCount', 1);
+    });
+
+    it("renders canvas", () => {
+        const wrapper = mount(el);
+        
+        expect(wrapper.find("canvas")).toHaveLength(1);
+    });
+	
+	it("changes mode", () => {
+		const wrapper: ReactWrapper<{}, WorkspaceState> = mount(el);
+
+		wrapper.setState({mode: "click"});
+		expect(wrapper.state().mode).toBe("click");
+	});
+
+});
