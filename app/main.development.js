ec0691fae94ef4bd0633170bf84ec9b71e24b7cd
TooFiveFive <git@er1c.me>
2019-02-17 19:26:12 +0000

    assets

--- app/main.development.js
+++ app/main.development.js
@@ -4,37 +4,37 @@ const {
 	Menu,
 	shell,
 	ipcMain
-} = require('electron');
+} = require("electron");
 
 let menu;
 let template;
 let mainWindow = null;
 let settingsWindow = null;
 
-if (process.env.NODE_ENV === 'production') {
-	const sourceMapSupport = require('source-map-support'); // eslint-disable-line
+if (process.env.NODE_ENV === "production") {
+	const sourceMapSupport = require("source-map-support"); // eslint-disable-line
 	sourceMapSupport.install();
 }
 
-if (process.env.NODE_ENV === 'development') {
-	require('electron-debug')(); // eslint-disable-line global-require
-	const path = require('path'); // eslint-disable-line
-	const p = path.join(__dirname, '..', 'app', 'node_modules'); // eslint-disable-line
-	require('module').globalPaths.push(p); // eslint-disable-line
+if (process.env.NODE_ENV === "development") {
+	require("electron-debug")(); // eslint-disable-line global-require
+	const path = require("path"); // eslint-disable-line
+	const p = path.join(__dirname, "..", "app", "node_modules"); // eslint-disable-line
+	require("module").globalPaths.push(p); // eslint-disable-line
 }
 
-app.on('window-all-closed', () => {
-	if (process.platform !== 'darwin') app.quit();
+app.on("window-all-closed", () => {
+	if (process.platform !== "darwin") app.quit();
 });
 
 
 const installExtensions = () => {
-	if (process.env.NODE_ENV === 'development') {
-		const installer = require('electron-devtools-installer'); // eslint-disable-line global-require
+	if (process.env.NODE_ENV === "development") {
+		const installer = require("electron-devtools-installer"); // eslint-disable-line global-require
 
 		const extensions = [
-			'REACT_DEVELOPER_TOOLS',
-			'REDUX_DEVTOOLS'
+			"REACT_DEVELOPER_TOOLS",
+			"REDUX_DEVTOOLS"
 		];
 		const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
 		return Promise.all(extensions.map(name => installer.default(installer[name], forceDownload)));
@@ -53,17 +53,17 @@ var settings = () => {
 
 	settingsWindow.loadURL(`file://${__dirname}/settings.html`);
 
-	settingsWindow.webContents.on('did-finish-load', () => {
+	settingsWindow.webContents.on("did-finish-load", () => {
 		settingsWindow.show();
 		settingsWindow.focus();
 	});
 
-	settingsWindow.on('closed', () => {
+	settingsWindow.on("closed", () => {
 		settingsWindow = null;
 	});
 }
 
-app.on('ready', () =>
+app.on("ready", () =>
 	installExtensions()
 	.then(() => {
 		mainWindow = new BrowserWindow({
@@ -74,25 +74,25 @@ app.on('ready', () =>
 
 		mainWindow.loadURL(`file://${__dirname}/app.html`);
 
-		mainWindow.webContents.on('did-finish-load', () => {
+		mainWindow.webContents.on("did-finish-load", () => {
 			mainWindow.show();
 			mainWindow.focus();
 		});
 
-		mainWindow.on('closed', () => {
+		mainWindow.on("closed", () => {
 			mainWindow = null;
 		});
 
-		if (process.env.NODE_ENV === 'development') {
+		if (process.env.NODE_ENV === "development") {
 			mainWindow.openDevTools();
-			mainWindow.webContents.on('context-menu', (e, props) => {
+			mainWindow.webContents.on("context-menu", (e, props) => {
 				const {
 					x,
 					y
 				} = props;
 
 				Menu.buildFromTemplate([{
-					label: 'Inspect element',
+					label: "Inspect element",
 					click() {
 						mainWindow.inspectElement(x, y);
 					}
