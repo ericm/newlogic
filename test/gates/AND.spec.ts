38ec17c5bcd32901af1e7d6075c0fd59daecdd1a
TooFiveFive <git@er1c.me>
2019-02-18 15:44:06 +0000

    added gates tests

---
+++ test/gates/AND.spec.ts
@@ -0,0 +1,21 @@
+import { GateCoords, GateSize } from './../../app/interfaces/canvas.d';
+import AndGate from "../../app/gates/AND";
+
+const canvas = document.createElement("canvas");
+const ctx = canvas.getContext("2d");
+
+describe("Gates", () => {
+    it("initiates", () => {
+        if (ctx !== null) {
+            const gate = new AndGate(ctx);
+            const coords: GateCoords = {x: 100, y: 100};
+            const size: GateSize = {width: 40, height: 40};
+            const wrapper = gate.add(coords, size);
+            expect(wrapper).toHaveProperty("start");
+            expect(wrapper).toHaveProperty("end");
+
+            expect(wrapper.end).toHaveLength(2);
+            expect(wrapper.start).toHaveLength(1);
+        }
+    });
+});
