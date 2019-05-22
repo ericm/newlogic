39733fecba8db7c8277fb0475b60444e67a2bf6b
Eric Moynihan <git@moynihan.io>
2019-05-22 17:41:36 +0100

    Added NOR option

--- app/gates/OR.ts
+++ app/gates/OR.ts
@@ -2,17 +2,24 @@ import * as canvas from '../interfaces/canvas';
 import Gates from './Gates';
 import GateNode from './Node';
 
-let img = require('../img/or.svg')
+let img = require('../img/or.svg');
+let imgNot = require('../img/orNot.svg');
 export default class OrGate extends Gates<OrGate> {
+
     public static SVG: HTMLImageElement
+    public static SVGNOT: HTMLImageElement
 
     public static LOAD = (ctx: CanvasRenderingContext2D): Promise<boolean> => new Promise<boolean>((resolve) => {
         const listen = (_: Event): void => resolve(true);
         OrGate.SVG = new Image();
         OrGate.SVG.src = img;
-
         ctx.drawImage(OrGate.SVG, 0, 0, 0, 0);
-        OrGate.SVG.addEventListener("load", listen);
+        OrGate.SVG.addEventListener("load", () => {
+            OrGate.SVGNOT = new Image();
+            OrGate.SVGNOT.src = imgNot;
+            ctx.drawImage(OrGate.SVGNOT, 0, 0, 0, 0);
+            OrGate.SVGNOT.addEventListener("load", listen);
+        });
     })
 
     public constructor(ctx: CanvasRenderingContext2D) {
@@ -22,6 +29,24 @@ export default class OrGate extends Gates<OrGate> {
 
         this.nodeOffsetStart = [{ x: 40, y: 20.5 }]
         this.nodeOffsetEnd = [{ x: 0, y: 1.5 }, { x: 0, y: 39.5 }]
+
+        this.contextMenu = ["Make NOR"].concat(this.contextMenu);
+    }
+
+    public checkCustomContext = (option: string): boolean => {
+        switch (option) {
+        case "Make NOR":
+            this.svg = OrGate.SVGNOT;
+            this.contextMenu[0] = "Make OR";
+            this.state.invert = true;
+            return true;
+        case "Make OR":
+            this.svg = OrGate.SVG;
+            this.contextMenu[0] = "Make NOR";
+            this.state.invert = false;
+            return true;
+        }
+        return false;
     }
 
     public add = (c: canvas.GateCoords, s: canvas.GateSize, id?: number): canvas.Nodes<any> => {
@@ -39,7 +64,8 @@ export default class OrGate extends Gates<OrGate> {
             },
             gateIn: new Array(),
             gateOut: new Array(),
-            id: !!id ? id : Gates.INCID()
+            id: !!id ? id : Gates.INCID(),
+            invert: false
         }
 
         this.render();
@@ -51,9 +77,12 @@ export default class OrGate extends Gates<OrGate> {
         this.upEval();
         
         // Bitwise OR
-        this.state.nodes.start[0].setVal(
-            this.state.nodes.end[0].getVal() || this.state.nodes.end[1].getVal()
-        );
+        let val = this.state.nodes.end[0].getVal() || this.state.nodes.end[1].getVal();
+        if (this.state.invert) {
+            val = !val;
+        }
+
+        this.state.nodes.start[0].setVal(val);
 
     }
 
