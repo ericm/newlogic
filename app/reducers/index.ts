03a5b2fd7e122bc5c98d9b982a3e49ac3bdfd541
Eric Moynihan <git@moynihan.io>
2019-05-29 12:59:44 +0100

    Removed unecessary boilerplate

--- app/reducers/index.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers, Reducer } from 'redux';
-import { routerReducer as routing } from 'react-router-redux';
-
-const rootReducer = combineReducers({
-  routing: routing as Reducer<any>
-});
-
-export default rootReducer;
