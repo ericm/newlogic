facc5d4163a23bf43dd68fdbb281a2147a316f31
Eric Moynihan <git@moynihan.io>
2019-06-05 17:25:56 +0100

    Release v0.1.4-beta

--- tar.sh
+++ tar.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-# Copyright Eric Moynihan
+# Copyright Eric Moynihan 2019, licensed under MIT
+# This file is used to package newlogic on linux
+npm run package
 cd ./release/linux-unpacked
 tar -cvzf ../newlogic-linux-$1.tar.gz *
