df60289e61eed80ddcc79f06940d1dd82aa1a403
Eric Moynihan <git@moynihan.io>
2019-06-06 19:30:53 +0100

    Improved install on linux

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 # Copyright Eric Moynihan 2019, licensed under MIT
 # This file is used to package newlogic on linux
+# Usage: ./install.sh <version>
 npm run package
 cd ./release/linux-unpacked
+cp ../../resources/icon.png .
+cp ../../install/newlogic.desktop .
 tar -cvzf ../newlogic-linux-$1.tar.gz *
